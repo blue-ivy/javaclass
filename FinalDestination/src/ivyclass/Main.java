f5719a1ff0ad8f555f3162efe1f1c0fcb5e7adbd
blue-ivy <ivydowling@protonmail.com>
2019-10-09 12:43:20 -0400

    sailing game we programmed together

--- FinalDestination/src/ivyclass/Main.java
+++ FinalDestination/src/ivyclass/Main.java
@@ -6,18 +6,44 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter username!");
+        System.out.println("What is your name?");
         String userName = scan.nextLine();
-        System.out.println("Username is: " + userName);
+        System.out.println("Nice to meet you, " + userName);
         String inputString = "";
-        while (!inputString.equals("quit")) {
-            System.out.println("Where would you like to go?");
+        System.out.println("Where would you like to sail?");
+        inputString = scan.nextLine();
+        System.out.println("You have begun your journey to " + inputString);
+        System.out.println(".");
+        System.out.println(".");
+        System.out.println(".");
+        while (inputString != "quit") {
+            System.out.println("You begin to feel hungry do you fish or look for shore?");
             inputString = scan.nextLine();
-            System.out.println(inputString + " is beautiful this time of year!");
-            if (Math.random() > 0.9) {
-                System.out.println("Unfortunately your plane was delayed so you won'y be making it to " + inputString);
-                inputString = "quit";
+            System.out.println("You try to " + inputString);
+            if (inputString.contains("fish")) {
+                System.out.println("Food finds you slowly, but you survive for now.");
+            } else {
+                if (Math.random() > 0.7) {
+                    // find land
+                    System.out.println("You find an island in the ocean.");
+                    System.out.println("Do you land or move on?");
+                    inputString = scan.nextLine();
+                    if (inputString.contains("land")) {
+                        if (Math.random() > 0.5) {
+                            System.out.println("It's a mirage, you fall off the edge of the world.");
+                            inputString = "quit";
+                        } else {
+                            System.out.println("You live happily ever after on a tropical island.");
+                        }
+                    } else {
+                        System.out.println("You sail on.");
+                    }
+                } else {
+                    // no land
+                    System.out.println("You find no land, but many sharks and dolphins.");
+                }
             }
         }
+
     }
 }
